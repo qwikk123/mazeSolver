6e33db35f4cc2e6943fadea26e0fff9ecb5795fd
qwikk <44299295+qwakk@users.noreply.github.com>
2023-06-07 11:45:50 +0200

    cleaned up structure and the prints in main

--- src/main/java/MazeSolver/MazeMain.java
+++ src/main/java/MazeSolver/MazeMain.java
@@ -1,35 +1,11 @@
 package MazeSolver;
 
-import MazeSolver.Maze;
-import MazeSolver.MazePair;
-import MazeSolver.MazeSolver;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class MazeMain {
     public static void main(String[] args) {
-        String mazeString1 = """
-                ##########
-                #S       #
-                ######## #
-                #        #
-                ######## #
-                # #  #   #
-                # #  #   #
-                #        #
-                #E########""";
-        String mazeString2 = """
-                ##########
-                #S       #
-                ######## #
-                #        #
-                ######## #
-                E #  #   #
-                # #  #   #
-                #        #
-                ##########""";
-        String mazeString3 = """
+        String mazeString = """
                 ################################
                 #S             #               #
                 ########       #               #
@@ -46,27 +22,17 @@ public class MazeMain {
                 #              #               #
                 ################################""";
         MazeSolver mazeSolver = new MazeSolver();
-        Maze maze1 = new Maze();
-        maze1.fillMaze(mazeString1);
-        Maze maze2 = new Maze();
-        maze2.fillMaze(mazeString2);
-        Maze maze3 = new Maze();
-        maze3.fillMaze(mazeString3);
+        Maze maze = new Maze();
+        maze.fillMaze(mazeString);
 
-
-        System.out.println(maze1);
-        System.out.println(mazeSolver.iterative(maze1));
         System.out.println("-----------------------------");
-        System.out.println(maze2);
-        System.out.println(mazeSolver.iterative(maze2));
-        System.out.println("-----------------------------");
-        System.out.println(maze3);
+        System.out.println(maze);
         List<MazePair> pathList = new ArrayList<>();
-        mazeSolver.DFS(maze3,maze3.getStartx(),maze3.getStarty(), pathList);
+        mazeSolver.DFS(maze,maze.getStartx(),maze.getStarty(), pathList);
         for (MazePair p : pathList) {
-            maze3.getSquare(p.y(),p.x()).setType('+');
+            maze.getSquare(p.y(),p.x()).setType('+');
         }
-        System.out.println(maze3);
+        System.out.println(maze);
         System.out.println("-----------------------------");
     }
 }
